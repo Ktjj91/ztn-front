78bbc848100e2c23566e9bb75f3225f2e52792ed
Ktjj91 <kingsleythomas33@gmail.com>
2025-05-09 15:00:12 +0200

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import {NoteEditorComponent} from '../components/note-editor/note-editor.component';
 
 export const routes: Routes = [
-  {
-    path: 'new-note',component:NoteEditorComponent
+  //new-note
+    {
+    path: '',component:NoteEditorComponent
   }
 ];
--- src/components/note-editor/note-editor.component.ts
+++ src/components/note-editor/note-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, signal, ViewChild} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MarkdownModule} from 'ngx-markdown';
 
@@ -9,6 +9,38 @@ import {MarkdownModule} from 'ngx-markdown';
   styleUrl: './note-editor.component.css'
 })
 export class NoteEditorComponent {
-  markdown = '';
+  markdown = signal<string>('');
+  @ViewChild('editor') editor!: ElementRef<HTMLTextAreaElement>;
+  insert(prefix: string,listOdered:string = ""): void {
+      let p:string;
+    if(listOdered >= "1") {
+      let n = parseInt(listOdered);
+      n++;
+       p = n.toString()
+      prefix = `${p}. `;
+
+    }
+    const textarea = this.editor.nativeElement;
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const before = this.markdown().slice(0, start);
+    const after = this.markdown().slice(end);
+    const selected = this.markdown().slice(start, end);
+
+    this.markdown.set(`${before}${prefix}${selected}${after}`);
+    textarea.focus();
+  }
+
+  wrap(wrapper: string): void {
+    const textarea = this.editor.nativeElement;
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const before = this.markdown().slice(0, start);
+    const after = this.markdown().slice(end);
+    const selected = this.markdown().slice(start, end);
+
+    this.markdown.set( `${before}${wrapper}${selected}${wrapper}${after}`);
+    textarea.focus();
+  }
 
 }
